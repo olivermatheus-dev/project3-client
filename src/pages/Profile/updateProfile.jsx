485499437491c5a7285955ce9915022495adedfb
Per00 <pedrohgbarbosa@hotmail.com>
2023-03-06 15:33:23 -0300

    updated-form

--- src/pages/Profile/updateProfile.jsx
+++ src/pages/Profile/updateProfile.jsx
@@ -1,3 +1,47 @@
-export function UpdateProfile() {
-  return <h1> funcionando </h1>;
+import { useState, useEffect } from "react";
+import { api } from "../../config/api/api.jsx";
+
+export function UpdateProfile({ setUpdated, updated }) {
+  const storedUser = localStorage.getItem("loggedInUser");
+  const [userForm, setUserForm] = useState({ name: "seu nome" });
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const response = await api.get("/user/profile");
+        setUserForm({ ...response.data });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchUser();
+  }, [updated]);
+
+  function handleChange(e) {
+    setUserForm({ ...userForm, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await api.put("/user/update", { ...userForm });
+      setUpdated((state) => {
+        !state;
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={userForm.name}
+          onChange={handleChange}
+        />
+        <button>clique aqui!</button>
+      </form>
+    </>
+  );
 }
