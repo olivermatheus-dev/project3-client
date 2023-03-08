7c8848d53f648d2e417f5b9b2dc997466a7d84ac
olivermnoo <riseumpro@gmail.com>
2023-03-08 02:47:09 -0300

    Searchbar otimizada darkmode

--- src/components/Navbar/searchbar.jsx
+++ src/components/Navbar/searchbar.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { apiNoToken } from "../../config/api/apiNoToken";
 import _ from "lodash";
+import { TabBoxNavbar } from "./tabboxnavbar";
 
 export function SearchBar() {
   const [search, setSearch] = useState("");
@@ -41,34 +42,42 @@ export function SearchBar() {
   }
 
   return (
-    <div
-      className="flex flex-col fixed mx-auto right-16 sm:right-40 md:right-72 top-1 w-36 sm:w-60 z-10 "
-      ref={searchBarRef}
-    >
-      <div className="w-28 sm:w-44 absolute">
-        <label
-          htmlFor="searchBar"
-          className="relative block overflow-hidden border-b border-gray-200 pt-3 focus-within:border-emerald-600"
-        >
-          <input
-            type="searchBar"
-            id="searchBar"
-            placeholder="searchBar"
-            className="text-slate-100 peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-            onChange={(e) => setSearch(e.target.value.replaceAll(" ", "%20"))}
-          />
+    <>
+      <div
+        className="flex flex-col fixed mx-auto right-16 sm:right-40 md:right-72 top-1 w-36 sm:w-60 z-50 "
+        ref={searchBarRef}
+      >
+        <div className="w-28 sm:w-44 absolute">
+          <label
+            htmlFor="searchBar"
+            className="relative block overflow-hidden border-b border-gray-200 pt-3 focus-within:border-emerald-600"
+          >
+            <input
+              type="searchBar"
+              id="searchBar"
+              placeholder="searchBar"
+              className="dark:text-zinc-100 text-zinc-800 peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+              onChange={(e) => setSearch(e.target.value.replaceAll(" ", "%20"))}
+            />
 
-          <span className="absolute left-0 top-2 -translate-y-1/2 text-xs text-emerald-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-placeholder-shown:text-emerald-600 peer-focus:text-emerald-800 peer-focus:top-2 peer-focus:text-xs">
-            <p>Search</p>
-          </span>
-        </label>
+            <span className="absolute left-0 top-2 -translate-y-1/2 text-xs text-emerald-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-placeholder-shown:text-emerald-600 peer-focus:text-emerald-800 peer-focus:top-2 peer-focus:text-xs">
+              <p>Search</p>
+            </span>
+          </label>
+        </div>
+        <div className=" h-auto mt-16 w-80 ">
+          {results.length > 0 && (
+            <div className="flex flex-col gap-2">
+              {results.map((currentElement) => {
+                return <TabBoxNavbar tab={currentElement} />;
+              })}
+            </div>
+          )}
+        </div>
       </div>
-      <div className="flex flex-col h-auto mt-16 w-60 gap-2">
-        {results.length > 0 &&
-          results.map((currentElement) => {
-            return <h1>{currentElement.title}</h1>;
-          })}
-      </div>
-    </div>
+      {results.length > 0 && (
+        <div className="fixed inset-0 bg-black/20 w-screen h-screen transition-all z-10"></div>
+      )}
+    </>
   );
 }
